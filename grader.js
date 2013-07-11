3be25c3246e5eda73c1f034d8d622bfaeb4aeca7
tathagata <tathagatadg@gmail.com>
2013-07-11 00:29:10 +0000

    Only web request works. The file check json commented out.

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +63,32 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkUrl = function(htmlfile,checks){
+    var writeToFile = function(result, response){
+	if (result instanceof Error){
+	    console.error('Error:' + util.format(respones.message));
+	}else{
+	    fs.writeFileSync(htmlfile, result);
+	    var checkJson = checkHtmlFile(htmlfile, checks);
+	    console.log(JSON.stringify(checkJson, null, 4));
+	}
+    };
+    return writeToFile;
+}
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to web', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    //var checkJson = checkHtmlFile(program.file, program.checks);
+    var writeToFile = checkUrl("tmp.html", program.checks);
+    rest.get(program.url).on('complete', writeToFile);
+    //var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
